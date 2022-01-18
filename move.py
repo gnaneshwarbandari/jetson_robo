92ba50ed57f72b9e5a1be63af994b2d3ba994a5c
Gnaneshwar Bandari <31314778+gnaneshwarbandari@users.noreply.github.com>
2022-01-18 16:10:46 +0530

    Update move.py

--- move.py
+++ move.py
@@ -1,15 +1,15 @@
 import Jetson.GPIO as GPIO
 import time
 
-w11 = 3
-w12 = 4
-w21 = 7 
-w22 = 11
+w11 = 11
+w12 = 7
+w21 = 13
+w22 = 15
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(w11, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(w12, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(w21, GPIO.OUT, initial=GPIO.LOW)
-GPIO.setup(w22, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(w11, GPIO.OUT)
+GPIO.setup(w12, GPIO.OUT)
+GPIO.setup(w21, GPIO.OUT)
+GPIO.setup(w22, GPIO.OUT)
 print("Initialized Done.....")
 
 while True:
@@ -27,20 +27,20 @@ while True:
 		GPIO.output(w12, GPIO.HIGH)
 		GPIO.output(w21, GPIO.LOW)
 		GPIO.output(w22, GPIO.HIGH)
-	elif s == 'right':
-		print("Turning Right..")
+	elif s == 'left':
+		print("Turning Left..")
 		GPIO.output(w11, GPIO.LOW)
 		GPIO.output(w12, GPIO.LOW)
 		GPIO.output(w21, GPIO.HIGH)
 		GPIO.output(w22, GPIO.LOW)
-	elif s == 'left':
-		print("Turning Left..")
+	elif s == 'right':
+		print("Turning Right..")
 		GPIO.output(w11, GPIO.HIGH)
 		GPIO.output(w12, GPIO.LOW)
 		GPIO.output(w21, GPIO.LOW)
 		GPIO.output(w22, GPIO.LOW)
 	elif s == 'stop':
-		print("Moving Forward..")
+		print("Stopping")
 		GPIO.output(w11, GPIO.LOW)
 		GPIO.output(w12, GPIO.LOW)
 		GPIO.output(w21, GPIO.LOW)
